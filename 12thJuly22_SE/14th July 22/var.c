22c9af160dcbd1cbb02eae677170b50abf5df490
sugandhsec <sugandhsec112@gmail.com>
2022-11-14 13:40:45 +0530

    firstly added

---
+++ 12thJuly22_SE/14th July 22/var.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    // <datatype> <var_name> = <value>;
+    char S[20] = "Hello World";   // declaration var
+    printf("%s\n",s);
+    printf("%s\n",s);
+    printf("%s\n",s);
+    printf("%s\n",s);
+    printf("%s\n",s);
+    printf("%s\n",s);
+    printf("%s\n",s);
+    printf("%s\n",s);
+    printf("%s\n",s);
+    printf("%s\n",s);
+    printf("%s\n",s);
+   
+    return 0;
+}
