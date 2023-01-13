971146701a353b491585f4b9e00c22a33bf6b32b
sugandhsec <sugandhsec112@gmail.com>
2023-01-13 16:59:27 +0530

    13-jan

--- 16 nov c c++/database/11-Jan-2023.sql
+++ 16 nov c c++/database/11-Jan-2023.sql
@@ -33,6 +33,43 @@ select sum(amount) from payments where customerNumber=103;
 select max(amount) from payments;
 select min(amount) from payments;
 select avg(amount) from payments;
-select count(customerNumber) from customers where city="NYC";
+select cocustomersunt(customerNumber) from customers where city="NYC";
 
 select * from customers where city="NYC";
+-- 13-Jan-2023
+
+select sum(amount) from payments where customerNumber=112;
+select sum(amount) from payments where customerNumber=114;
+select customerNumber,sum(amount) from payments group by customernumber;
+select customerNumber ,max(amount) from payments group by customernumber;
+
+select customerNumber,sum(amount) from payments group by customerNumber having sum(amount)>50000;
+select customerNumber,sum(amount) from payments group by customerNumber having sum(amount)<50000;
+--  in limit first values is starting point and next value is number of records
+select * from customers limit 10;
+select * from customers limit 11,10;
+select * from customers limit 21,30;
+
+-- TCL(TRANSACTION CONTROL LANGUAGE)
+-- rollback; queries effect will be reversed
+
+use college;
+
+start transaction;
+savepoint a;
+insert into course(course_name,due,studentfees) values("MCA",588,487500);
+insert into course(course_name,due,studentfees) values("BSCIT",588,487500);
+insert into course(course_name,due,studentfees) values("MSCIT",588,487500);
+savepoint b;
+insert into course(course_name,due,studentfees) values("CA",588,487500);
+insert into course(course_name,due,studentfees) values("MBBS",588,487500);
+insert into course(course_name,due,studentfees) values("TC",588,487500);
+
+rollback to b;
+
+commit;
+
+DCL(DATA CONTROL LANGUAGE)
+
+grant
+revoke
